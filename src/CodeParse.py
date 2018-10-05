76f74c4eb8f3b093bcc8758224ba0abb4400265c
McAJBen <McAJBen@gmail.com>
2018-10-05 17:29:52 -0400

    Added DividerType class

--- src/CodeParse.py
+++ src/CodeParse.py
@@ -9,6 +9,16 @@ class Segment:
         self.data = code[prev_div.pos:cur_div.pos + len(prev_div.end)]
         self.category = prev_div.start
 
+# used to tell which each divider should look for
+class DividerType:
+    start = str()
+    end = str()
+
+    # constructor
+    def __init__(self, start = "", end = ""):
+        self.start = start
+        self.end = end
+
 # used to tell that a symbol is at a position in a string
 class Divider:
     pos = int()
@@ -16,10 +26,10 @@ class Divider:
     end = str()
 
     # constructor
-    def __init__(self, pos, start, end):
+    def __init__(self, pos = 0, divider_type = DividerType()):
         self.pos = pos
-        self.start = start
-        self.end = end
+        self.start = divider_type.start
+        self.end = divider_type.end
 
     # returns true if self is less than other
     def __lt__(self, other):
@@ -28,25 +38,34 @@ class Divider:
 # code is a string of C++
 # returns a list of Dividers, one for each instance of a symbol
 def find_all_dividers(code):
+    divider_types = {
+        DividerType("//", "\n"),
+        DividerType("/*", "*/"),
+        DividerType("*/", ""),
+        DividerType("\n", ""),
+        DividerType("\"", "\""),
+        DividerType("\'", "\'")
+    }
     dividers = list()
-    to_end = {"//":"\n", "/*":"*/", "\"":"\"", "\'":"\'", "*/":"", "\n":""}
-    for symbol in {"//", "/*", "*/", "\n"}:
-        dividers += find_dividers(code, symbol, to_end[symbol])
-    for symbol in {"\"", "\'"}:
-        pos_dividers = find_dividers(code, symbol, to_end[symbol])
-        dividers += (d for d in pos_dividers if not is_escaped(code, d.pos))
+    for divider_type in divider_types:
+        if (divider_type.start in {"//", "/*", "*/", "\n"}):
+            dividers += find_dividers(code, divider_type)
+    for divider_type in divider_types:
+        if (divider_type.start in {"\"", "\'"}):
+            possible_dividers = find_dividers(code, divider_type)
+            dividers += (d for d in possible_dividers if not is_escaped(code, d.pos))
     dividers.sort()
     return dividers
 
 # code is a string of C++
 # symbol is a string to look for
 # returns a list of Dividers, one for each instance of symbol
-def find_dividers(code, symbol, end):
+def find_dividers(code, divider_type):
     dividers = list()
-    index = code.find(symbol, 0)
+    index = code.find(divider_type.start, 0)
     while (0 <= index and index < len(code)):
-        dividers.append(Divider(index, symbol, end))
-        index = code.find(symbol, index + 1)
+        dividers.append(Divider(index, divider_type))
+        index = code.find(divider_type.start, index + 1)
     return dividers
 
 # code is a string of C++
@@ -63,7 +82,7 @@ def is_escaped(code, index):
 def find_segments(code):
     segments = list()
     dividers = find_all_dividers(code)
-    prev_divider = Divider(0, "", "")
+    prev_divider = Divider()
     for divider in dividers:
         if (prev_divider.start == ""):
             if (divider.start in {"//", "/*", "\""} or
@@ -73,10 +92,8 @@ def find_segments(code):
         else:
             if (divider.start == prev_divider.end):
                 segments.append(Segment(code, prev_divider, divider))
-                prev_divider.pos = divider.pos + len(divider.start)
-                prev_divider.start = ""
-                prev_divider.end = ""
-    segments.append(Segment(code, prev_divider, Divider(len(code), "", "")))
+                prev_divider = Divider(divider.pos + len(divider.start))
+    segments.append(Segment(code, prev_divider, Divider(len(code))))
     return segments
 
 # returns C++ code from file at path with escaped new lines removed
