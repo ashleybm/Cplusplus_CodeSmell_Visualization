a23c42711970ad09890105d1d5bfe7b6a4a5b056
McAJBen <McAJBen@gmail.com>
2018-11-19 20:15:17 -0500

    Updated Comment Style

--- src/CodeParse.py
+++ src/CodeParse.py
@@ -1,50 +1,74 @@
 
-# used to split a string into segments of different meaning
 class Segment:
-    data = str()
-    category = str()
+    """
+    used to split a string into segments of different meaning
+        data - chracters from file that are in this segment
+        category - how the characters should be dealt with
+        '' - undefined code (internal use only)
+        'H' - header
+        'S' - symbol
+        'W' - word
+        'N' - number literal
+        '/*' or '//' - comment
+        '"' - string literal
+        ''' - character literal
+    """
 
-    # constructor
     def __init__(self, category, data):
         self.category = category
         self.data = data
 
-# used to tell which each divider should look for
 class DividerType:
-    start = str()
-    end = str()
-    expand = int()
+    """
+    a type of divider that starts a new segment of code
+        start - starting character
+        end - ending character
+        expand - does this divider expand into the end character
+    """
 
-    # constructor
     def __init__(self, start = "", end = "", expand = 0):
         self.start = start
         self.end = end
         self.expand = expand
 
-# used to tell that a symbol is at a position in a string
 class Divider(DividerType):
-    pos = int()
+    """
+    used to tell that a symbol is at a position in a string
+        pos - the position of this divider
+        start - the starting symbol
+        end - the ending symbol
+        expand - does this divider expand into the end character
+    """
 
-    # constructor
     def __init__(self, pos = 0, divider_type = DividerType()):
         self.pos = pos
         self.start = divider_type.start
         self.end = divider_type.end
         self.expand = divider_type.expand
 
-    # returns true if self is less than other
     def __lt__(self, other):
+        """
+        overrides < op, returns true if self is less than other
+        """
         return self.pos < other.pos
 
-# used to manage segments and split further into words
 class SegmentManager:
-    segments = list()
-    overhang = str()
+    """
+    used to manage segments and split further into words
+        segments - individual segments between each type of code
+        overhand - remainder segment not completed
+    """
 
-    # code is a string of C++
-    # category is the type of code
-    # splits the code into multiple segments if necessary and stores them as seperate segments
+    def __init__(self):
+        self.segments = list()
+        self.overhang = str()
+
     def append(self, code, category):
+        """
+        splits the code into multiple segments if necessary and stores them as seperate segments
+            code - a string of C++
+            category - the type of code {"", "//", "/*", "\"", "\'"}
+        """
         if (category == ""):
             code = self.overhang + code
             self.overhang = str()
@@ -54,12 +78,13 @@ class SegmentManager:
         else:
             self.segments.append(Segment(category, code))
 
-    # code is a string of C++
-    # index is the next character to check
-    # adds the segment starting at index to segments
-    # returns the index of the next character to check
     def append_code(self, code, index):
-        # TODO add in reserved word dictionary?
+        """
+        adds the segment starting at index to segments,
+        returns the index of the next character to check
+            code - string of C++
+            index - next character to check
+        """
         if (code[index] in {" ", "\t", "\n"}):
             return index + 1
         elif (code[index] == "#"):
@@ -85,32 +110,41 @@ class SegmentManager:
             self.segments.append(Segment("S", code[index]))
             return index + 1
 
-    # code is a string of C++
-    # index is the first character
-    # method is a method that returns true if you are still in the segment
-    # symbol is the category of this segment
-    # finds the end of a word or number
-    # returns the index of the next character to check
+
     def add_group(self, code, index, method, symbol):
+        """
+        finds the end of a word or number,
+        returns the index of the next character to check
+            code - a string of C++
+            index - the first character
+            method - a method that returns true if you are still in the segment
+            symbol - the category of this segment
+        """
         end_index = index + 1
         while (end_index < len(code) and method(code[end_index])):
             end_index += 1
         self.segments.append(Segment(symbol, code[index:end_index]))
         return end_index
 
-# char is a character
-# returns true if char can be in a variable name
 def is_word(char):
+    """
+    returns true if char can be in a variable name
+        char - a character
+    """
     return char.isalnum() or char == "_"
 
-# char is a character
-# returns true if char can be in a number
 def is_num(char):
+    """
+    returns true if char can be in a number
+        char - a character
+    """
     return char.isalnum() or char in {"'", "."}
 
-# code is a string of C++
-# returns a list of Dividers, one for each instance of a symbol
 def find_all_dividers(code):
+    """
+    returns a list of Dividers, one for each isntance of a symbol
+        code - a string of C++
+    """
     divider_types = {
         DividerType("//", "\n", 0),
         DividerType("/*", "*/", 2),
@@ -130,10 +164,12 @@ def find_all_dividers(code):
     dividers.sort()
     return dividers
 
-# code is a string of C++
-# divider_type is the divider to search for
-# returns a list of Dividers, one for each instance of symbol
 def find_dividers(code, divider_type):
+    """
+    returns a list of Dividers, one for each instance of symbol
+        code - a string of C++
+        divider_type - the divider to search for
+    """
     dividers = list()
     index = code.find(divider_type.start, 0)
     while (0 <= index and index < len(code)):
@@ -141,18 +177,22 @@ def find_dividers(code, divider_type):
         index = code.find(divider_type.start, index + 1)
     return dividers
 
-# code is a string of C++
-# index is the index of a character that could be escaped
-# returns true if there is an odd number of backslashes before index
 def is_escaped(code, index):
+    """
+    returns true if there is an odd number of backslashes before index
+        code - a string of C++
+        index - the index for a character that could be escaped
+    """
     prev_index = index - 1
     while (prev_index >= 0 and code[prev_index] == "\\"):
         prev_index -= 1
     return (index - prev_index) % 2 == 0
 
-# path is a .cpp file
-# returns a list of Segments, one for each group of code in the same category
 def find_segments(path):
+    """
+    returns a list of Segments, one for each group of code in the same category
+        path - a .cpp file
+    """
     code = get_code(path)
     segment_manager = SegmentManager()
     dividers = find_all_dividers(code)
@@ -170,15 +210,19 @@ def find_segments(path):
     segment_manager.append(code[prev_divider.pos:], prev_divider.start)
     return segment_manager.segments
 
-# returns C++ code from file at path with escaped new lines removed
 def get_code(path):
+    """
+    returns C++ code from file at path with escaped new lines removed
+    """
     code = open(path, 'r').read()
     code = code.replace("\\\n", "")
     return code
 
-# segments is a list of Segment
-# prints all of the segments with what type of segment
 def print_all_segments(segments):
+    """
+    prints all of the segments with what type of segment they are
+        segments - list of Segments
+    """
     for seg in segments:
         category = str()
         if (seg.category == ""):
@@ -201,9 +245,11 @@ def print_all_segments(segments):
             category = seg.category
         print(category, ":", seg.data.replace("\n", " "))
 
-# segments is a list of Segment
-# prints the minimal amount needed to still have runnable code
 def print_min_segments(segments):
+    """
+    prints the minimal amount needed to still have runnable code
+        segments - list of Segments
+    """
     prev_is_word = False
     line_empty = True
     for seg in segments:
